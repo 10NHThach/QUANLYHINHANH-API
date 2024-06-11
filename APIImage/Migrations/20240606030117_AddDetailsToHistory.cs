798ddca538a20990a00112ddea52c07d0b8c0f0f
Courses <nguyenhuuthach1310@gmail.com>
2024-06-11 12:42:17 +0700

    api-image

---
+++ APIImage/Migrations/20240606030117_AddDetailsToHistory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIImage.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDetailsToHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Histories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Histories");
+        }
+    }
+}
